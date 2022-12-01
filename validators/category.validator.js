835ed3d883460d2782fac995298e64bab3a50263
unknown <cuongnv0920@gmail.com>
2022-12-01 13:59:46 +0700

    done category

--- validators/category.validator.js
+++ validators/category.validator.js
@@ -8,19 +8,45 @@ const validatorCreate = () => {
       .isEmpty()
       .withMessage("Vui lòng nhập Tên danh mục")
       .custom((value) => {
-        return Category.exists({ name: value }).then((category) => {
-          if (category) {
-            return Promise.reject(
-              "Tên danh mục đã tồn tại, vui lòng nhập Tên danh múc khác."
-            );
-          }
-        });
+        return Category.exists({ name: value })
+          .where({ softDelete: "" })
+          .then((category) => {
+            if (category) {
+              return Promise.reject(
+                "Tên danh mục đã tồn tại, vui lòng nhập Tên danh múc khác."
+              );
+            }
+          });
+      }),
+  ];
+};
+
+const validatorUpdate = () => {
+  return [
+    check("name")
+      .not()
+      .isEmpty()
+      .withMessage("Vui lòng nhập Tên danh mục")
+      .custom((value, { req }) => {
+        return Category.findOne({
+          _id: { $ne: req.params.id },
+          name: { $eq: value },
+        })
+          .where({ softDelete: "" })
+          .then((category) => {
+            if (category) {
+              return Promise.reject(
+                "Tên danh mục đã tồn tại, vui lòng nhập Tên danh múc khác."
+              );
+            }
+          });
       }),
   ];
 };
 
 const validator = {
   validatorCreate,
+  validatorUpdate,
 };
 
 module.exports = validator;
